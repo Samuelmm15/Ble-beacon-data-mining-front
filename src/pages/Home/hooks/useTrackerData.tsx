b59a9ddbde13bc3b329dca280e8c242247e3363e
Samuel <alu01013595262ull.edu.es>
2024-04-23 19:40:42 +0100

    feat: useTracketData hook

---
+++ src/pages/Home/hooks/useTrackerData.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const useTrackerData = () => {
+
+  async function getTracker() {
+    const res = await fetch("http://localhost:3000/api/trackerData/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "charset": "utf-8",
+      },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    } else {
+      throw new Error("Failed to fetch tracker data");
+    }
+  }
+
+  return {
+    getTracker,
+  };
+};
+
+export default useTrackerData;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useTrackerData from "./hooks/useTrackerData";
 
 const Home = () => {
+  const { getTracker } = useTrackerData();
+  
+  useEffect(() => {
+    getTracker().then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.error('Error fetching tracker data:', error);
+    });
+  });
+
   return (
     <div>
       <h1>Example Home Page</h1>
