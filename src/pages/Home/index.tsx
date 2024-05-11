ab303cf1a966e9ec991a39450054cef96bc07131
Samuel <alu01013595262ull.edu.es>
2024-05-11 12:09:30 +0100

    feat: first prototype home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,21 +3,66 @@ import useTrackerData from "./hooks/useTrackerData";
 import NavBar from "./components/NavBar";
 
 const Home = () => {
-  const { getTracker } = useTrackerData();
+  // const { getTracker } = useTrackerData();
 
-  useEffect(() => {
-    getTracker()
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching tracker data:", error);
-      });
-  });
+  // useEffect(() => {
+  //   getTracker()
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching tracker data:", error);
+  //     });
+  // });
 
   return (
     <div>
       <NavBar />
+      <section className="h-screen w-full bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1473968512647-3e447244af8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-gray-700 bg-blend-multiply">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+            Soar towards innovation: BLE and drones taking technology to new
+            heights.
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+            Our application immerses you in the exciting world of BLE technology
+            applied to drones. With BLE, drones become powerful tracking tools,
+            allowing you to locate and monitor beacons and transmitters from
+            anywhere. Join us in this thrilling technological adventure and
+            discover how we are transforming the way we interact with our
+            environment.
+          </p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a
+              href="http://localhost:3001/map"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            >
+              Get started
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+            <a
+              href="#"
+              className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
