64608ac739bf4cb13922cd00080e837285467370
Samuel <alu01013595262ull.edu.es>
2024-05-11 13:28:35 +0100

    feat: login prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
+import LogIn from "./pages/LogIn/inex";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/about" element={<h1>About</h1>} />
           <Route path="/map" element={<Map />} />
           <Route path="/dataBase" element={<h1>Data Base</h1>} />
+          <Route path="/login" element={<LogIn />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   const logo =
     require("../../img/real-time-tracking-logo-circular.png") as string;
 
   return (
-    <nav className="bg-navbar-color bg-opacity-59 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-navbar-color bg-opacity-56 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="http://localhost:3001/"
@@ -15,12 +17,14 @@ const NavBar = () => {
           </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <Link to={"/login"}>
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Log In
           </button>
+          </Link>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
