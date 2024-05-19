2f11b03e013fac3e2e44185b8af94a7d99c23541
Samuel <alu01013595262ull.edu.es>
2024-05-19 18:07:44 +0100

    feat: protected map and data base routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import Map from "./pages/Map";
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
 import { useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
 function App() {
+  const ProtectedElement = ({ element }: { element: React.ReactElement }) => {
+    const location = useLocation();
+    const isAuthenticated = localStorage.getItem("token") !== null;
+
+    return isAuthenticated ? (
+      element
+    ) : (
+      <Navigate to="/login" replace state={{ from: location }} />
+    );
+  };
+
   return (
     <div className="App bg-custom-color min-h-screen">
       <BrowserRouter>
@@ -13,8 +25,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/dataBase" element={<h1>Data Base</h1>} />
+          <Route path="/map" element={<ProtectedElement element={<Map />} />} />
+          <Route
+            path="/dataBase"
+            element={<ProtectedElement element={<h1>Data Base</h1>} />}
+          />
           <Route path="/login" element={<LogIn />} />
           <Route path="/register" element={<Register />} />
         </Routes>
