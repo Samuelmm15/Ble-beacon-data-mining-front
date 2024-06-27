54441031d30803dc78668f5fde22840792214572
Samuel <alu01013595262ull.edu.es>
2024-06-27 20:06:37 +0100

    wip: delete sig up option at sign in

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -113,15 +113,6 @@ const LogIn = () => {
                 >
                   Sign in
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
-                  <a
-                    href="/register"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Sign up
-                  </a>
-                </p>
               </form>
             </div>
           </div>
